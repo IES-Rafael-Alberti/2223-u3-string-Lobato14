6fc5642cf1ffd15191762a16ef075f07004d5d92
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-10-30 12:16:59 +0100

    Creación y realización de Ejercicio_4.py

---
+++ src/Ejercicio_4.py
@@ -0,0 +1,12 @@
+# Hay un método de cadenas llamado count que es similar a find. Lee la documentación 
+# de este método en: * Métodos en ingles * Métodos en castellano y escribe el 
+# código necesario para invocar a este método y contar el número de veces que una 
+# letra aparece en “banana”.
+
+# Entrada
+cadena = "banana"
+letra = "a"
+# Proceso
+ocurrencias = cadena.count(letra)
+# Salida
+print(f"El número de ocurrencias de '{letra}' en '{cadena}' es: {ocurrencias}")
