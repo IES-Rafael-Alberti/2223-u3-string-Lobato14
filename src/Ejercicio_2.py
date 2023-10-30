fe492b697e953c71ae5785fa25d25c35cc77e254
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-10-30 11:35:25 +0100

    Creacion y realización del Ejercicio_2.py

---
+++ src/Ejercicio_2.py
@@ -0,0 +1,8 @@
+# Dado que fruta es una variable de tipo cadena, ¿qué significa fruta[:]?
+
+# Significa que devolvera el valor de la cadena proporcionada en dicha variable
+# como se muestra en el siguiente codigo:
+
+fruta = "manzana"
+subcadena = fruta[:]
+print(subcadena)
